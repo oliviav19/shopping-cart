fd36947bb6dd8a9fd551f75c27c49af67e6f4b46
Olivia Vitali <orv208@stern.nyu.edu>
2021-06-13 18:44:14 -0400

    User Input

--- shopping_cart.py
+++ shopping_cart.py
@@ -34,8 +34,17 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 
+#REQUIREMENTS
 
-print(len(products))
+#A grocery store name of your choice
+#A grocery store phone number and/or website URL and/or address of choice
+#The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+#The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+#The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+#The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+#The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+#A friendly message thanking the customer and/or encouraging the customer to shop again
+
 
 # 1) capture product ids until we're done
 # (use infinite while loop)
@@ -43,7 +52,7 @@ print(len(products))
 selected_ids = []
 
 while True:
-    selected_id = input("Please select / scan a valid product id: ")
+    selected_id = input("Please select or scan a valid product ID, otherwise type 'DONE' when complete: ")
     if selected_id.upper() == "DONE":
         break
     else:
