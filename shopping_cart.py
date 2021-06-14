1c0e85313abe09c1fac89b837d914c308ea738c0
Olivia Vitali <orv208@stern.nyu.edu>
2021-06-13 23:02:42 -0400

    adding validation statement v1

--- shopping_cart.py
+++ shopping_cart.py
@@ -62,6 +62,12 @@ while True:
         #print("Selected Product: ", matching_product["name"], matching_product["price"])
         selected_ids.append(selected_id) #write a list of products after typing DONE
 
+if (selected_id == [selected_ids] or "DONE"):
+    print("Valid Entry")
+else: 
+    print("OOPS, one of your inputs is an incorrect product ID. Please try again.")
+    exit()
+
 print("---------------------------------")
 print("Westside Market")
 print("Address: 77 Seventh Avenue, New York, NY 10011")
@@ -95,37 +101,3 @@ print("Total:" , to_usd(total_total))
 print("---------------------------------")
 print("Thanks for shopping at your neighborhood Westside Market! We hope to see you again soon!")
 
-##sending email 
-import os
-from dotenv import load_dotenv
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-
-load_dotenv()
-
-SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
-SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
-
-client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
-print("CLIENT:", type(client))
-
-subject = "Your Receipt from the Green Grocery Store"
-
-html_content = "Hello World"
-print("HTML:", html_content)
-
-# FYI: we'll need to use our verified SENDER_ADDRESS as the `from_email` param
-# ... but we can customize the `to_emails` param to send to other addresses
-message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
-
-try:
-    response = client.send(message)
-
-    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
-    print(response.status_code) #> 202 indicates SUCCESS
-    print(response.body)
-    print(response.headers)
-
-except Exception as err:
-    print(type(err))
-    print(err)
